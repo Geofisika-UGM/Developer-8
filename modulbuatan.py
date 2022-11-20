9b60ba72c9eb7aab0735b7acce896812749fa3e1
Takwaokeda <takwa.okeda2602@mail.ugm.ac.id>
2022-11-20 18:14:48 +0700

    tambahan

--- modulbuatan.py
+++ modulbuatan.py
@@ -12,13 +12,15 @@ def wavelet(f,t):
 
 #Impedansi akustik
 def impedansi(rho,v):
+    '''Rho atau massa jenis(ρ) adalah pengukuran massa setiap satuan volume benda
+     kecepatan (v) adalah jarak perpindahan suatu objek dalam waktu tertentu'''
     rho, v = np.array(rho, dtype=float), np.array(v, dtype=float)
     return rho*v
 
 #Koefisien Seismik Refleksi
 def koefisien_refleksi(rho,v):
     rho, v = np.array(rho, dtype=float), np.array(v, dtype=float)
-    Z   = rho*v       # impedansi akustik
+    Z   = rho*v #Impedansi akustik
     dZ  = (Z[1:] - Z[:-1])
     sZ  = (Z[:-1] + Z[1:])
     R   = dZ/sZ #Koefisien refleksi
